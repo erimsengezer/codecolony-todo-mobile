5f6b97cf4804927cf2b09797acc3630e61c526c5
erimsengezer <erimsengezer@gmail.com>
2019-07-13 15:50:25 +0300

    Initial Commit

---
+++ codecolony-todoapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codecolony-todoapp
+//
+//  Created by Erim Şengezer on 13.07.2019.
+//  Copyright © 2019 Erim Şengezer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
